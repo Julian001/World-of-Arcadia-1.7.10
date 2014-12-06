3b9062d0e922221a219601d5780b7989215ddd04
Julian001 <Julian.Heusler@hotmail.de>
2014-12-06 19:13:00 +0100

    Fix

--- src/main/java/com/julian001/arcadia/blocks/BlockAnvilArcadia.java
+++ src/main/java/com/julian001/arcadia/blocks/BlockAnvilArcadia.java
@@ -28,9 +28,8 @@ public class BlockAnvilArcadia extends BlockArcadia implements ITileEntityProvid
 	
 	@Override
 	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int meta, float hitX, float hitY, float hitZ) {
-       if(world.isRemote) {
-            if (world.getTileEntity(x, y, z) != null)
-                player.openGui(arcadia.instance, GUIs.ANVILARCADIA.ordinal(), world, x, y, z);
+       if(!world.isRemote) {
+            player.openGui(arcadia.instance, GUIs.ANVILARCADIA.ordinal(), world, x, y, z);
             return true;
         }
         return true;
